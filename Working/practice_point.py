53236f2bbd0493030c8dcc3d3e638812f9b8aa86
I-Lin <ian.lin20@ycdsbk12.ca>
2019-03-20 19:19:48 -0400

    Complete practice_point.py

--- Working/practice_point.py
+++ Working/practice_point.py
@@ -1,1 +1,19 @@
-class Point()
+class Point(object):
+
+    def __init__(self, x_pos, y_pos):
+        self.x = x_pos
+        self.y = y_pos
+
+    def get_distance(self, other_point):
+        distance = ((self.x - other_point.x)**2 + (self.y - other_point.y)**2) ** 0.5
+        return distance
+
+
+def main():
+    p1 = Point(1, 1)
+    p2 = Point(4, 5)
+
+    print("The distance between the 2 points is", Point.get_distance(p1, p2))
+
+
+main()
