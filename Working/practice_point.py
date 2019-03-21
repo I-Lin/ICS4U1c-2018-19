7e520192e0c4825dde7fd92eb61f2ff644958a8f
I-Lin <ian.lin20@ycdsbk12.ca>
2019-03-21 09:47:39 -0400

    Edit practice_point.py

--- Working/practice_point.py
+++ Working/practice_point.py
@@ -1,15 +1,28 @@
 class Point(object):
 
     def __init__(self, x_pos, y_pos):
+        """
+        Create an instance of a Point
+        :param x_pos: x coordinate value
+        :param y_pos: y coordinate value
+        """
         self.x = x_pos
         self.y = y_pos
 
     def get_distance(self, other_point):
+        """
+        Compute the distance between the current object and another Point object
+        :param other_point: Point object to find the distance to
+        :return: float
+        """
         distance = ((self.x - other_point.x)**2 + (self.y - other_point.y)**2) ** 0.5
         return distance
 
 
 def main():
+    """
+    Program demonstrating the creation of Point instances and calling class methods
+    """
     p1 = Point(1, 1)
     p2 = Point(4, 5)
 
